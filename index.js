d00689f5b82132ed5fcd1ffe54f928b79caecfe1
seol <hhhseolah@gmail.com>
2023-05-05 10:13:14 +0900

    my first app server

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express" //express 라이브러리 불러옴
+const app= express() //const 상수
+
+const port= 8080
+
+app.listen(port, console.log("server started"))
+
+//코드 빌드 순서
+//js java python ruby swift ...
+//1. 위에서 아래 방향
+//2. = 기준으로 오른쪽에서 왼쪽으로 치환
+//3. . 하위 메소드 호출
+//4. , 그리고로 해석
+//5. () 함수의 모음을 해석
